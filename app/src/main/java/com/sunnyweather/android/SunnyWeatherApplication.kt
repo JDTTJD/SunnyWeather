e7b321617831187f81b5a004aac0db90d109f622
JDTTJD <820231422@qq.com>
2020-07-11 17:57:47 +0800

    获取天气数据逻辑层代码完成，token待补充

---
+++ app/src/main/java/com/sunnyweather/android/SunnyWeatherApplication.kt
@@ -0,0 +1,25 @@
+package com.sunnyweather.android
+
+import android.annotation.SuppressLint
+import android.app.Application
+import android.content.Context
+
+/**
+ *
+ *Create by JDT on 2020-07-11.
+ *
+ */
+class SunnyWeatherApplication : Application() {
+    companion object {
+
+        const val TOKEN = ""
+
+        @SuppressLint("StaticFieldLeak")
+        lateinit var context: Context
+    }
+
+    override fun onCreate() {
+        super.onCreate()
+        context = applicationContext
+    }
+}
---
+++ app/src/main/java/com/sunnyweather/android/logic/model/PlaceResponse.kt
@@ -0,0 +1,24 @@
+package com.sunnyweather.android.logic.model
+
+import android.location.Address
+import android.location.Location
+import com.google.gson.annotations.SerializedName
+
+/**
+ *
+ *Create by JDT on 2020-07-11.
+ *
+ */
+class PlaceResponse {
+    data class PlaceResponse(val status: String, val places: List<Place>)
+
+    data class Place(
+        val name: String,
+        val location: Location,
+        @SerializedName("formatted_address")
+        val address: String
+    )
+
+    data class Location(val lng: String, val lat: String)
+
+}
